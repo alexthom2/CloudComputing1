4ea3a936c14917754bec87d296a305f6bc9391ec
theparttimeanalyst <alex.thom25@gmail.com>
2022-01-21 10:11:26 +0000

    final edit

--- CloudPerf/app.R
+++ CloudPerf/app.R
@@ -9,6 +9,50 @@
 
 
 
+
+
+## packages used 
+library(tidyverse)
+
+library(lubridate)
+
+## reading the data for the dashboard 
+setwd("~/Data Science Masters/cloudcomputing/data")
+
+gpu <- read_csv("gpu.csv")
+
+appcheck <- read_csv("application-checkpoints.csv")
+
+task <- read_csv("task-x-y.csv")
+
+
+
+## calculating the time for the full render 
+
+host_us <- appcheck %>%  filter(eventName == "TotalRender")  %>%
+  arrange(timestamp) %>%
+  group_by(hostname, taskId) %>%
+  mutate(rown = 1:n()) %>%
+  filter(rown == 1) 
+
+
+
+host_us_end <- appcheck %>%  filter(eventName == "TotalRender") %>%
+  arrange(timestamp) %>%
+  group_by(hostname, taskId) %>%
+  mutate(rown = 1:n()) %>%
+  slice_max(rown) %>%
+  select(timestamp, hostname, taskId)
+
+
+colnames(host_us_end)[1] <- "timeend"
+
+
+data <- host_us %>% left_join(host_us_end, by = c("hostname", "taskId")) %>%
+  mutate(duration = timeend - timestamp)
+
+### calculating the saving config time 
+
 host_us <- appcheck %>%  filter(eventName == "Saving Config")  %>%
   arrange(timestamp) %>%
   group_by(hostname, taskId) %>%
@@ -35,7 +79,7 @@ data_conf <- host_us %>% left_join(host_us_end, by = c("hostname", "taskId")) %>
 
 colnames(data_conf)[4] <- "Sconf_t"
 
-
+## calculating the tiling time 
 host_us <- appcheck %>%  filter(eventName == "Tiling")  %>%
   arrange(timestamp) %>%
   group_by(hostname, taskId) %>%
@@ -69,7 +113,7 @@ host_us <- appcheck %>%  filter(eventName == "Render")  %>%
   mutate(rown = 1:n()) %>%
   filter(rown == 1) 
 
-
+## calculating the render time 
 
 
 host_us_end <- appcheck %>%  filter(eventName == "Render") %>%
@@ -88,6 +132,8 @@ data_rend <- host_us %>% left_join(host_us_end, by = c("hostname", "taskId")) %>
   select(timestamp, hostname, taskId, duration)
 
 colnames(data_rend)[4] <- "render_t"
+
+## calculating the uploadingf time 
 
 host_us <- appcheck %>%  filter(eventName == "Uploading")  %>%
   arrange(timestamp) %>%
@@ -116,7 +162,7 @@ data_up <- host_us %>% left_join(host_us_end, by = c("hostname", "taskId")) %>%
 
 colnames(data_up)[4] <- "upload_t"
 
-
+### putting the data together 
 
 
 shiny_dat1 <-  data %>% left_join(data_conf, by = c( "hostname", "taskId")) %>% 
@@ -126,7 +172,7 @@ shiny_dat1 <-  data %>% left_join(data_conf, by = c( "hostname", "taskId")) %>%
   left_join(task, by = "taskId")
 
 
-
+## finding the average time for each host and filtering for the 10 slowest 
 host_av <- shiny_dat1 %>% group_by(hostname) %>% 
   summarise(meandur = mean(duration)) %>% 
   mutate(rank = rank(desc(meandur))) 
@@ -135,24 +181,24 @@ shiny_dat2 <- shiny_dat1 %>% left_join(host_av, by = "hostname") %>%
                               filter(rank < 11)
 
 
-
+## preparing the dat for the shiny graphs 
 shiny_dat3 <- shiny_dat2 %>% 
   select(timestamp.x, hostname, taskId, duration, Sconf_t, render_t, tiling_t, upload_t) %>% 
   pivot_longer(cols = 4:8, names_to = "stage", values_to = "time")
 
-
+# colours used. 
 cols <- c("a" = "#bd4b00", "b" = "#16004a")
 
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+
 ui <- fluidPage(
 
     # Application title
     titlePanel("Cloud GPU Performance"),
 
-    # Sidebar with a slider input for number of bins 
+ # side bar to select the gpu from the 10 slowest  
     sidebarLayout(
         sidebarPanel(
             selectInput("GPUinp",
@@ -160,7 +206,7 @@ ui <- fluidPage(
                         choices = shiny_dat3$hostname)
         ),
        
-        # Show a plot of the generated distribution
+        # 2 plots outputted by the dashboard 
         mainPanel(
            plotOutput("plot1"), 
           plotOutput("plot2"))
@@ -168,7 +214,7 @@ ui <- fluidPage(
 ) 
 
 
-# Define server logic required to draw a histogram
+# server code to output the 2 plots 
 server <- function(input, output) {
 
   output$plot1 <- renderPlot({
@@ -179,6 +225,7 @@ server <- function(input, output) {
              
     ggplot(highlighted, aes(x = stage, y= time, col = sel)) + geom_jitter(size = 3) + 
                                               facet_wrap(~stage, scales = "free") +
+                                                labs(title = "TIme taken For Each Stage of Render") + 
                                                 scale_color_manual(values = cols) +
                                                     theme(panel.background = element_blank())
              
@@ -192,7 +239,9 @@ server <- function(input, output) {
     selected <- shiny_dat3 %>% filter(hostname == input$GPUinp) %>% 
                                     filter(stage == "duration")
     
-    ggplot(selected, aes(x = timestamp.x, y = time, col = stage)) + geom_line()
+    ggplot(selected, aes(x = timestamp.x, y = time, col = stage)) + geom_line(col = "#bd4b00") +
+                                                  labs(x = "Time", y = "Render Time (s)", title= "Node Render Time Trend") + 
+                                                    theme(panel.background = element_blank())
     
   })
 
